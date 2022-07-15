90c2b2d2771689186c08419f8be744d37dc066d4
Michael Adalikwu <michaeladalikwu@gmail.com>
2022-07-15 16:51:50 +0100

    Recommended Food Page UI Implementation

---
+++ lib/pages/food/recommended_food_details.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/utils/colors.dart';
+import 'package:food_delivery_app/utils/dimensions.dart';
+import 'package:readmore/readmore.dart';
+
+import '../../widgets/app_icon.dart';
+import '../../widgets/big_text.dart';
+
+class RecommendedFoodDetails extends StatelessWidget {
+  const RecommendedFoodDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            toolbarHeight: scaleHeight(70),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                AppIcon(icon: Icons.close),
+                AppIcon(icon: Icons.shopping_cart),
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(20),
+              child: Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.symmetric(vertical: scaleHeight(10)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(scaleWidth(20)),
+                      topRight: Radius.circular(scaleWidth(20))),
+                ),
+                child: Center(child: BigText(text: 'Chinese Quisine')),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
+            expandedHeight: scaleHeight(250),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                "assets/images/food3.jpg",
+                width: double.maxFinite,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+                margin: EdgeInsets.only(
+                    left: scaleWidth(20),
+                    right: scaleWidth(20),
+                    top: scaleHeight(10)),
+                child: Column(
+                  children: [
+                    Container(
+                      child: ReadMoreText(
+                        'Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Let\'s try to understand the concept about all products and services uploaded here they are all good products. Take a deep dive into the logics required to store information in the database as information generated are pushed ahead to take columns and rows.',
+                        trimLines: 7,
+                        colorClickableText: Colors.pink,
+                        trimMode: TrimMode.Line,
+                        trimCollapsedText: ' Show more',
+                        trimExpandedText: ' Show less',
+                        style: TextStyle(
+                            fontSize: scaleHeight(14),
+                            height: 2.0,
+                            color: AppColors.mainBlackColor),
+                        moreStyle: TextStyle(
+                            fontSize: scaleHeight(14),
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.mainColor),
+                        lessStyle: TextStyle(
+                            fontSize: scaleHeight(14),
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.mainColor),
+                      ),
+                    )
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
