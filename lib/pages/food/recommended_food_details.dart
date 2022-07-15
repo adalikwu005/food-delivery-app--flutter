47bd18389f7bddcf32fd6bf1473b4ab718a56376
Michael Adalikwu <michaeladalikwu@gmail.com>
2022-07-15 23:45:00 +0100

    Recommended Food Details Screen (Bottom NavBar UI Implementation)

--- lib/pages/food/recommended_food_details.dart
+++ lib/pages/food/recommended_food_details.dart
@@ -56,32 +56,104 @@ class RecommendedFoodDetails extends StatelessWidget {
                     right: scaleWidth(20),
                     top: scaleHeight(10)),
                 child: Column(
+                  //Full Details food screen info
                   children: [
-                    Container(
-                      child: ReadMoreText(
-                        'Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Let\'s try to understand the concept about all products and services uploaded here they are all good products. Take a deep dive into the logics required to store information in the database as information generated are pushed ahead to take columns and rows.',
-                        trimLines: 7,
-                        colorClickableText: Colors.pink,
-                        trimMode: TrimMode.Line,
-                        trimCollapsedText: ' Show more',
-                        trimExpandedText: ' Show less',
-                        style: TextStyle(
-                            fontSize: scaleHeight(14),
-                            height: 2.0,
-                            color: AppColors.mainBlackColor),
-                        moreStyle: TextStyle(
-                            fontSize: scaleHeight(14),
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.mainColor),
-                        lessStyle: TextStyle(
-                            fontSize: scaleHeight(14),
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.mainColor),
-                      ),
+                    ReadMoreText(
+                      'Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Flutter is Google mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase. Let\'s try to understand the concept about all products and services uploaded here they are all good products. Take a deep dive into the logics required to store information in the database as information generated are pushed ahead to take columns and rows.',
+                      trimLines: 10,
+                      //trimLength: 200,
+                      //colorClickableText: Colors.pink,
+                      trimMode: TrimMode.Line,
+                      trimCollapsedText: ' Show more',
+                      trimExpandedText: ' Show less',
+                      style: TextStyle(
+                          fontSize: scaleHeight(14),
+                          height: 2.0,
+                          color: AppColors.mainBlackColor),
+                      moreStyle: TextStyle(
+                          fontSize: scaleHeight(14),
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.mainColor),
+                      lessStyle: TextStyle(
+                          fontSize: scaleHeight(14),
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.mainColor),
                     )
                   ],
                 )),
           )
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: scaleWidth(50), vertical: scaleHeight(10)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  icon: Icons.remove,
+                  backgroundColor: AppColors.mainColor,
+                  size: scaleHeight(50),
+                  iconColor: Colors.white,
+                  iconSize: scaleHeight(24),
+                ),
+                BigText(
+                  text: '\$${12.88} x ${0}',
+                  size: scaleHeight(20),
+                ),
+                AppIcon(
+                  icon: Icons.add,
+                  iconColor: Colors.white,
+                  size: scaleHeight(50),
+                  backgroundColor: AppColors.mainColor,
+                  iconSize: scaleHeight(24),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: scaleHeight(100),
+            padding: EdgeInsets.symmetric(
+              horizontal: scaleWidth(15),
+              vertical: scaleHeight(15),
+            ),
+            decoration: BoxDecoration(
+              color: AppColors.buttonBackgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(scaleHeight(20)),
+                topRight: Radius.circular(scaleHeight(20)),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //Favourite Button
+                Container(
+                    padding: EdgeInsets.all(scaleHeight(15)),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(scaleWidth(20))),
+                    child: const Icon(
+                      Icons.favorite,
+                      color: AppColors.mainColor,
+                    )),
+                //Add to Cart Button
+                Container(
+                  padding: EdgeInsets.all(scaleHeight(15)),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(scaleWidth(20)),
+                      color: AppColors.mainColor),
+                  child: BigText(
+                    text: "\$10 | Add to Cart",
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
